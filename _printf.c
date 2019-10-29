99df0a0a1b3a24de51547d6eb7e13b3fad19459b
JuanDavidDava2 <juandm417@gmail.com>
2019-10-29 03:54:33 +0000

    fix betty

--- _printf.c
+++ _printf.c
@@ -17,25 +17,25 @@ int _printf(const char *format, ...)
 	va_start(ap, format);
 	if (format == NULL)
 	{
-	return (-1);
+		return (-1);
 	}
 	while (*(format + i))
 	{
 		if (*(format + i) == '%')
 		{
 			if (*(format + i + 1) == '\0')
-			return (-1);
+				return (-1);
 			else if (*(format + i + 1) == '%')
 			{
-			write(1, (format + i), 1);
-			i++;
+				write(1, (format + i), 1);
+				i++;
 			}
 			else if (*(format + i + 1) != 'c' && *(format + i + 1) != 's')
-                        write(1, (format + i), 1);
+				write(1, (format + i), 1);
 			else
 			{
-			type(format + i + 1)(ap);
-			i++;
+				type(format + i + 1)(ap);
+				i++;
 			}
 		}
 		else
